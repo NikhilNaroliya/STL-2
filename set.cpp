63630bc4c5ea87c405b35a3e74752cc21d978759
nikhil naroliya <nikhilnaroliya7exams@gmail.com>
2022-12-14 16:06:09 +0530

    added stl

--- set.cpp
+++ set.cpp
@@ -1,27 +1,35 @@
-#include<iostream>
 #include<bits/stdc++.h>
+
 using namespace std;
 
-int main(){
-    unordered_set<int>s;
-    s.insert(1);
-    s.insert(2);
-    int k=0;
-     for(int i=0;i<5;i++)
-      {
-        s.insert(i);
-      }
-      cout<<"the max element an unordered set can hold "<<s.max_size()<<endl;
-      cout<<"the max count of bucket is "<<s.max_bucket_count()<<endl;
-      cout<<"bicket size is "<<s.bucket_size(3)<<endl;
-      s.clear();
-    if(s.find(0)!=s.end())
-     {
-        cout<<k<<"  is present in the list \n"<<endl;
-     }
-     else
-     cout<<"nahi hai re bava\n";
-     cout<<"iski size  hai "<<s.size()<<endl;
-     cout<<s.empty();
-     return 0 ;
+int main() {
+  set < int > s;
+  for (int i = 1; i <= 10; i++) {
+    s.insert(i);
+  }
+
+  cout << "Elements present in the set: ";
+  for (auto it = s.begin(); it != s.end(); it++) {
+    cout << * it << " ";
+  }
+  cout << endl;
+  int n = 2;
+  if (s.find(2) != s.end())
+    cout << n << " is present in set" << endl;
+
+  s.erase(s.begin());
+  cout << "Elements after deleting the first element: ";
+  for (auto it = s.begin(); it != s.end(); it++) {
+    cout << * it << " ";
+  }
+  cout << endl;
+
+  cout << "The size of the set is: " << s.size() << endl;
+
+  if (s.empty() == false)
+    cout << "The set is not empty " << endl;
+  else
+    cout << "The set is empty" << endl;
+  s.clear();
+  cout << "Size of the set after clearing all the elements: " << s.size();
 }
